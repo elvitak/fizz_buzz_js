a032743c1292587600bb5a563bdc12053def9add
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-21 09:12:19 +0200

    add spec for FizzBuzz class

---
+++ spec/fizzBuzz.spec.js
@@ -0,0 +1,16 @@
+const subject = new FizzBuzz
+
+desribe("FizzBuzz", () => {
+    it("is expected to return a number if none of the game rules apply", () => {
+        expect(subject.check(1)).toEqual(1)
+    })
+    it("is expected to return 'fizz' if the number is divisable by 3", () => {
+        expect(subject.check(3)).toEqual("fizz")
+    })
+    it("is expected to return 'buzz' if the number is divisable by 5", () => {
+        expect(subject.check(5)).toEqual("buzz")
+    })
+    it("is expected to return 'fizz-buzz' if the number is divisable by 15", () => {
+        expect(subject.check(15)).toEqual("fizz-buzz")
+    })
+});
